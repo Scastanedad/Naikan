e09c3e71aa3700e6766a5f43e7003605a947b0e3
Scastanedad <scastanedad@uninorte.edu.co>
2026-04-10 13:14:38 -0500

    .Bat modificado
    Automatizacion de instalacion de windows y dependencias para desarrollo del juego

--- setup.bat
+++ setup.bat
@@ -1,6 +1,15 @@
 @echo off
 echo Instalando proyecto Naikan...
 
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python no esta instalado. Descargandolo...
+    curl -o python_installer.exe https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe
+    echo Instalando Python, acepta los permisos que aparezcan...
+    python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+    del python_installer.exe
+)
+
 python -m venv venv
 call venv\Scripts\activate
 pip install -r requirements.txt
