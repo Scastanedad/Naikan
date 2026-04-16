ae8fc71dc862bf2f825ace105e15939579a10024
Scastanedad <scastanedad@uninorte.edu.co>
2026-04-16 18:14:03 -0500

    DashFuncional
    Hicimos pruebas para habilitar un dashFuncional, no esta habilitado pero quedo en los comentarios

--- entidades/ET_jugador.py
+++ entidades/ET_jugador.py
@@ -6,10 +6,12 @@ class Jugador(Entidad):
     def __init__(self, x, y, vida= None, velocidad= None, width= None, heigth = None):
         super().__init__(x, y, vida= 3, velocidad=300, width=20, heigth=20)
         self.direccion= (1,0)
+        self.cooldown = 2
+        self.intervalo = 2
     
     #El movimiento asociado a las teclas
     def mover(self,dt,keys, width, height):
-        
+        self.cooldown +=dt
         if (keys[pygame.K_w] or keys[pygame.K_UP])and (self.y >0):
             self.y -= self.velocidad * dt 
             self.direccion = (0,-1)
@@ -22,6 +24,13 @@ class Jugador(Entidad):
         if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and (self.x >0) :
             self.x -= self.velocidad * dt
             self.direccion = (-1,0)
+
+        #DashFuncional
+        #if(keys[pygame.K_c] and (self.cooldown>=self.intervalo)):
+        #    self.cooldown = 0
+        #    self.x +=100*self.direccion[0]
+        #    self.y+=100*self.direccion[1]
+        
         self.actualizarRect()
        
     def recibirDaño(self, Daño=None):
