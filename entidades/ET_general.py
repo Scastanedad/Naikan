634daffaf338a961ecf3fb34f6a74ab1154f1ac0
SebastianBlanco430 <fsblanco@uninorte.edu.co>
2026-04-28 15:35:27 -0500

    Filtro aplicado a los proyectiles
    Se aplicó el filtro a los proyectiles. Realmente hay que tener muchísimo cuidado con los colores escogidos, porque pareciera por momentos en algunos filtros y con ciertos colores, que estos desaparecen, que no están ahí.

--- entidades/ET_general.py
+++ entidades/ET_general.py
@@ -35,12 +35,37 @@ class Proyectil(pygame.sprite.Sprite):
         self.direccion = direccion
         self.width = 5
         self.height = 5
-        self.image = pygame.Surface((self.width,self.height))
+        
+        self.color_original = color
+        self.color_actual = color
+        
+        from escenas.workModules.filtros import Filtros
+        Filtros.unirse_lista(self)
+
+        self.preparar_visuales()
+        
+        """ self.image = pygame.Surface((self.width,self.height))
         self.image.fill((color))
-        self.rect = self.image.get_rect(center=(self.x,self.y))
+        self.rect = self.image.get_rect(center=(self.x,self.y)) """
+        
         self.grace_period = 0.1  # 100ms sin colisionar
         self.t = 0
 
+    def preparar_visuales(self):
+        self.image = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.image.fill(self.color_actual)
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+
+    def configurar_filtro(self, nuevo_filtro):
+        from escenas.workModules.filtros import Filtros
+        
+        super_temp = pygame.Surface((1, 1), pygame.SRCALPHA)
+        super_temp.fill(self.color_original)
+        
+        self.color_actual = Filtros.aplicar_filtro(super_temp, nuevo_filtro).get_at((0, 0))
+        
+        self.preparar_visuales()
+        
     def update (self,dt):
         self.t += dt
         self.grace_period -= dt
@@ -61,4 +86,6 @@ class Proyectil(pygame.sprite.Sprite):
                 self.y += dt * self.velocidad * self.direccion[1] + perp_y * offset
         if self.rect.right < 0 or self.rect.left > 800:
             self.kill()
+            from escenas.workModules import Filtros
+            Filtros.quitarse_lista(self)
         self.rect.center = (self.x, self.y)
