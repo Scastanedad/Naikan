b73024e71f76c52133aa773c4d3d7651d9342335
SebastianBlanco430 <fsblanco@uninorte.edu.co>
2026-05-18 16:26:11 -0500

    Me arrepentí del g_utils

--- G_utils.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_path, relative_path)
