205283485b572b416fdbb20dc10d73cda2ece362
Samuel Castañeda <scastanedad@uninorte.edu.co>
2026-05-19 17:09:18 -0500

    Boss 3

--- entidades/enemigos/__init__.py
+++ entidades/enemigos/__init__.py
@@ -3,3 +3,4 @@ from entidades.enemigos.ET_E_distancia import EnemigoDistancia
 from entidades.enemigos.ET_E_Boss1 import Boss1
 from entidades.enemigos.ET_E_Boss2 import Boss2
 from entidades.enemigos.ET_E_miniBoss2 import miniBoss2
+from entidades.enemigos.ET_E_Boss3 import Boss3
